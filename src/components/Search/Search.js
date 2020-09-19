8befdaa1e054b15386750a8c333f6992b4752e3b
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-09-19 21:37:44 +0300

    Fix Search after review

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,38 +1,38 @@
 import React, { useState, useCallback } from 'react'
 
 const Search = () => {
-    const [value, setValue] = useState('')
+  const [value, setValue] = useState('')
 
-    const handleChange = useCallback(
-        (e) => {
-            setValue(e.target.value);
-        },
-        []
-    )
+  const handleChange = useCallback(
+    (e) => {
+      setValue(e.target.value);
+    },
+    []
+  )
 
-    const handleClick = useCallback(
-        () => {
-            let url = `https://sdamgia-homework-backend.herokuapp.com/api/search?query=${value}`
+  const handleClick = useCallback(
+    () => {
+      let url = `https://sdamgia-homework-backend.herokuapp.com/api/search?query=${value}`
 
-            fetch(url)
-                .then(response => response.json())
-                .then(data => console.log(data.subject));
-            console.log(value)
-        },
-        [value]
-    )
+      fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data.subject));
+      console.log(value)
+    },
+    [value]
+  )
 
-    return (
-        <div>
-            <input
-                type="text"
-                placeholder="Searching..."
-                value={value}
-                onChange={handleChange}
-            />
-            <button onClick={handleClick}>Поиск</button>
-        </div>
-    )
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Searching..."
+        value={value}
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Поиск</button>
+    </div>
+  )
 }
 
 export default Search

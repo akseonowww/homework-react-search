b53e206da68956775e2785aae7b7d5729c41577f
Ruslan Aksenov <akseonov.ru@ya.ru>
2020-09-19 20:14:48 +0300

    Started work with data of API

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from 'react'
 
 const Search = () => {
     const [value, setValue] = useState('')
+    let url = `https://sdamgia-homework-backend.herokuapp.com/api/search?query=${value}`
 
     const handleChange = useCallback(
         (e) => {
@@ -9,12 +10,16 @@ const Search = () => {
         },
         []
     )
-
+    
     const handleClick = useCallback(
         () => {
+            let promise = fetch(url)
+            promise.then(promise => promise.json())
+            promise.then(commits => console.log(commits.subject));
             console.log(value)
+            // console.log(promise)
         },
-        [value]
+        [value, url]
     )
 
     return (
